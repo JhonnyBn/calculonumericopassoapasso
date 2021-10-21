b415ce4de042f63717f90e323c4142bfba758984
Joao Neto <jhonneto1221@gmail.com>
2021-10-21 15:04:10 -0300

    fix bisseccao

--- js/bisseccao.js
+++ js/bisseccao.js
@@ -23,8 +23,8 @@ function bisseccao() {
 				return
 			}
 			let x = math.round((a+b)*0.5, p)
-			elementos.push([iteracao, a, b, x, sinal(f(a)), sinal(f(b)), sinal(f(x)), math.round(b-x, p), math.round(f(x), p)])
-			if ((b-x) < err)
+			elementos.push([iteracao, a, x, b, sinal(f(a)), sinal(f(x)), sinal(f(b)), math.round(a-b, p), math.round(f(x), p)])
+			if ((a-b) < err)
 				break
 			f(a)*f(x) < 0 ? b = x : a = x
 			iteracao += 1
@@ -32,7 +32,7 @@ function bisseccao() {
 	} catch(e) {}
 	document.getElementById("iteracaoBisseccao").value = 1
 	document.getElementById("divIteracaoBisseccao").style.display = ''
-	let cabecalho = ["Iteração", "a", "b", "c", "f(a)", "f(b)", "f(c)", "b-c", "f(c)"]
+	let cabecalho = ["Iteração", "a", "c", "b", "f(a)", "f(c)", "f(b)", "a-b", "f(c)"]
 	let opcoes = [{ "name": "Mostrar Gráfico", "action": "graficoBisseccaoDaLinha(this)" }]
 	tabela('tabelaBisseccao', cabecalho, elementos, opcoes)
 	show("tabelaBisseccao")
