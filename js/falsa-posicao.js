5b9813629a2ae7a75e81f4e143b912cd2bcaee95
Joao Neto <jhonneto1221@gmail.com>
2021-10-25 17:22:40 -0300

    adicionado js da falsa posicao e secante

---
+++ js/falsa-posicao.js
@@ -0,0 +1,125 @@
+function sinal(numero) {
+	return numero >= 0 ? '+' : '-'
+}
+
+function falsaPosicao() {
+	const expressao = document.getElementById('expr').value
+	const funcao = math.parse(expressao)
+	const funcaoCompilada = funcao.compile()
+	let f = (x) => { return funcaoCompilada.evaluate({x: x}) }
+	let elementos = []
+	let iteracao = 0
+	let a = document.getElementById("inicioFalsaPosicao").value
+	let b = document.getElementById("fimFalsaPosicao").value
+	let p = document.getElementById("precisaoFalsaPosicao").value
+	try{
+		p = math.round(math.parse(p).evaluate(), 0)
+		err = math.parse('10^-' + p).evaluate()
+		a = math.round(math.parse(a).evaluate(), p)
+		b = math.round(math.parse(b).evaluate(), p)
+		while(true) {
+			if(iteracao > 1000) {
+				alert('Mais de 1000 iteracoes')
+				return
+			}
+			let x = math.round(((a*f(b)-b*f(a))/(f(b)-f(a))), p)
+			if(iteracao < 50) {
+				console.log(iteracao, a, x, b, f(a), f(x), f(b), err)
+			}
+			elementos.push([iteracao, a, x, b, sinal(f(a)), sinal(f(x)), sinal(f(b)), math.round(a-x, p), math.round(b-x, p), math.round(f(x), p)])
+			if (math.abs(b-x) < err || math.abs(a-x) < err) {
+				break
+			}
+			f(a)*f(x) < 0 ? b = x : a = x
+			iteracao += 1
+		}
+	} catch(e) { console.log(e) }
+	document.getElementById("iteracaoFalsaPosicao").value = 0
+	document.getElementById("divIteracaoFalsaPosicao").style.display = ''
+	let cabecalho = ["Iteração", "a", "c", "b", "f(a)", "f(c)", "f(b)", "a-c", "b-c", "f(c)"]
+	let opcoes = [{ "name": "Mostrar Gráfico", "action": "graficoFalsaPosicaoDaLinha(this)" }]
+	tabela('tabelaFalsaPosicao', cabecalho, elementos, opcoes)
+	show("tabelaFalsaPosicao")
+	clearZoom()
+	graficoFalsaPosicaoDaIteracao()
+}
+
+function atualizarIteracaoFalsaPosicao(delta) {
+	const iteracao = document.getElementById('iteracaoFalsaPosicao')
+	const max = document.querySelectorAll("#tabelaFalsaPosicao > table > tbody > tr").length - 1
+	let iteracaoN = parseInt(iteracao.value)
+	if( iteracaoN + delta >= 0 && iteracaoN + delta <= max )
+	{
+		iteracao.value = iteracaoN + delta
+		iteracao.onchange()
+	}
+}
+
+function iteracaoChangeFalsaPosicao() {
+	const iteracao = document.getElementById('iteracaoFalsaPosicao').value
+	const max = document.querySelectorAll("#tabelaFalsaPosicao > table > tbody > tr").length - 1
+	if( iteracao < 0 )
+		document.getElementById('iteracaoFalsaPosicao').value = 0
+	if ( iteracao > max )
+		document.getElementById('iteracaoFalsaPosicao').value = max
+	graficoFalsaPosicaoDaIteracao()
+}
+
+function graficoFalsaPosicaoDaIteracao() {
+	const expressao = document.getElementById('expr').value
+	const funcao = math.parse(expressao)
+	const funcaoCompilada = funcao.compile()
+	let f = (x) => { return funcaoCompilada.evaluate({x: x}) }
+	const inicio = document.getElementById('inicio').value
+	const fim = document.getElementById('fim').value
+	const iteracao = parseInt(document.getElementById('iteracaoFalsaPosicao').value)
+	const resultado = document.querySelectorAll("#tabelaFalsaPosicao > table > tbody > tr:nth-child(" + ( iteracao + 1)  + ") > td")
+	const a = parseFloat(resultado[1].textContent)
+	const c = parseFloat(resultado[2].textContent)
+	const b = parseFloat(resultado[3].textContent)
+	const pontos = [
+		{nome: 'a', x: a},
+		{nome: 'c', x: c},
+		{nome: 'b', x: b}
+	]
+	let traces = [{
+		x: [a, b],
+		y: [f(a), f(b)], 	
+		type: 'lines'
+	}, {
+		x: [a, a],
+		y: [0, f(a)],
+		type: 'lines',
+		name: '',
+		line: {
+			color: 'gray',
+			dash: 'dot'
+		}
+	}, {
+		x: [b, b],
+		y: [0, f(b)],
+		type: 'lines',
+		name: '',
+		line: {
+			color: 'gray',
+			dash: 'dot'
+		}
+	}, {
+		x: [c, c],
+		y: [0, f(c)],
+		type: 'lines',
+		name: '',
+		line: {
+			color: 'gray',
+			dash: 'dot'
+		}
+	}]
+	graficoFx('plotFalsaPosicao', expressao, [inicio, fim], pontos, traces)
+}
+
+function graficoFalsaPosicaoDaLinha(elem) {
+	const iteracao = parseInt(elem.parentElement.parentElement.children[0].innerText)
+	document.getElementById("iteracaoFalsaPosicao").value = iteracao
+	graficoFalsaPosicaoDaIteracao()
+	window.scrollTo(0, 200)
+}
---
+++ js/secante.js
@@ -0,0 +1,125 @@
+function sinal(numero) {
+	return numero >= 0 ? '+' : '-'
+}
+
+function secante() {
+	const expressao = document.getElementById('expr').value
+	const funcao = math.parse(expressao)
+	const funcaoCompilada = funcao.compile()
+	let f = (x) => { return funcaoCompilada.evaluate({x: x}) }
+	let elementos = []
+	let iteracao = 0
+	let x0 = document.getElementById("inicioSecante").value
+	let x1 = document.getElementById("fimSecante").value
+	let p = document.getElementById("precisaoSecante").value
+	try{
+		p = math.round(math.parse(p).evaluate(), 0)
+		err = math.parse('10^-' + p).evaluate()
+		x0 = math.round(math.parse(x0).evaluate(), p)
+		x1 = math.round(math.parse(x1).evaluate(), p)
+		while(true) {
+			if(iteracao > 1000) {
+				alert('Mais de 1000 iteracoes')
+				return
+			}
+			let x2 = math.round((x1-f(x1)*((x1-x0)/(f(x1)-f(x0)))), p)
+			if(iteracao < 50) {
+				console.log(iteracao, x0, x2, x1, f(x0), f(x2), f(x1), err)
+			}
+			elementos.push([iteracao, x0, x2, x1, sinal(f(x0)), sinal(f(x2)), sinal(f(x1)), math.round(x0-x2, p), math.round(x1-x2, p), math.round(f(x2), p)])
+			if (math.abs(x1-x2) < err || math.abs(x0-x2) < err) {
+				break
+			}
+			f(x0)*f(x2) < 0 ? x1 = x2 : x0 = x2
+			iteracao += 1
+		}
+	} catch(e) { console.log(e) }
+	document.getElementById("iteracaoSecante").value = 0
+	document.getElementById("divIteracaoSecante").style.display = ''
+	let cabecalho = ["Iteração", "x0", "x2", "x1", "f(x0)", "f(x2)", "f(x1)", "x0-x2", "x1-x2", "f(x2)"]
+	let opcoes = [{ "name": "Mostrar Gráfico", "action": "graficoSecanteDaLinha(this)" }]
+	tabela('tabelaSecante', cabecalho, elementos, opcoes)
+	show("tabelaSecante")
+	clearZoom()
+	graficoSecanteDaIteracao()
+}
+
+function atualizarIteracaoSecante(delta) {
+	const iteracao = document.getElementById('iteracaoSecante')
+	const max = document.querySelectorAll("#tabelaSecante > table > tbody > tr").length - 1
+	let iteracaoN = parseInt(iteracao.value)
+	if( iteracaoN + delta >= 0 && iteracaoN + delta <= max )
+	{
+		iteracao.value = iteracaoN + delta
+		iteracao.onchange()
+	}
+}
+
+function iteracaoChangeSecante() {
+	const iteracao = document.getElementById('iteracaoSecante').value
+	const max = document.querySelectorAll("#tabelaSecante > table > tbody > tr").length - 1
+	if( iteracao < 0 )
+		document.getElementById('iteracaoSecante').value = 0
+	if ( iteracao > max )
+		document.getElementById('iteracaoSecante').value = max
+	graficoSecanteDaIteracao()
+}
+
+function graficoSecanteDaIteracao() {
+	const expressao = document.getElementById('expr').value
+	const funcao = math.parse(expressao)
+	const funcaoCompilada = funcao.compile()
+	let f = (x) => { return funcaoCompilada.evaluate({x: x}) }
+	const inicio = document.getElementById('inicio').value
+	const fim = document.getElementById('fim').value
+	const iteracao = parseInt(document.getElementById('iteracaoSecante').value)
+	const resultado = document.querySelectorAll("#tabelaSecante > table > tbody > tr:nth-child(" + ( iteracao + 1)  + ") > td")
+	const x0 = parseFloat(resultado[1].textContent)
+	const x2 = parseFloat(resultado[2].textContent)
+	const x1 = parseFloat(resultado[3].textContent)
+	const pontos = [
+		{nome: 'x0', x: x0},
+		{nome: 'x2', x: x2},
+		{nome: 'x1', x: x1}
+	]
+	let traces = [{
+		x: [x0, x1],
+		y: [f(x0), f(x1)], 	
+		type: 'lines'
+	}, {
+		x: [x0, x0],
+		y: [0, f(x0)],
+		type: 'lines',
+		name: '',
+		line: {
+			color: 'gray',
+			dash: 'dot'
+		}
+	}, {
+		x: [x1, x1],
+		y: [0, f(x1)],
+		type: 'lines',
+		name: '',
+		line: {
+			color: 'gray',
+			dash: 'dot'
+		}
+	}, {
+		x: [x2, x2],
+		y: [0, f(x2)],
+		type: 'lines',
+		name: '',
+		line: {
+			color: 'gray',
+			dash: 'dot'
+		}
+	}]
+	graficoFx('plotSecante', expressao, [inicio, fim], pontos, traces)
+}
+
+function graficoSecanteDaLinha(elem) {
+	const iteracao = parseInt(elem.parentElement.parentElement.children[0].innerText)
+	document.getElementById("iteracaoSecante").value = iteracao
+	graficoSecanteDaIteracao()
+	window.scrollTo(0, 200)
+}
